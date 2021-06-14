d4e5207faeec69531addb4cec0ad639e93082c9f
Jake Spain <jake.spain+15749776@puppet.com>
2021-06-14 16:37:35 -0400

    Updated for systemd module dependency

--- spec/defines/app_spec.rb
+++ spec/defines/app_spec.rb
@@ -17,6 +17,21 @@ describe 'unicorn::app' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to contain_service('unicorn_namevar') }
+
+      case os_facts[:service_provider]
+      when 'systemd'
+        it { is_expected.to contain_systemd__unit_file('unicorn_namevar.service') }
+      else
+
+        case os_facts[:kernel]
+        when 'Linux'
+          it { is_expected.to contain_file('/etc/default/unicorn_namevar') }
+          it { is_expected.to contain_file('/etc/init.d/unicorn_namevar') }
+        when 'freebsd'
+          it { is_expected.to contain_file('/usr/local/etc/rc.d/unicorn_namevar') }
+        end
+      end
 
       context 'with export_home => undef' do
         let(:params) do
